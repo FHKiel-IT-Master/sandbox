f128e0dcf857c385667f371b890f840d95c216e7
Marco Neumann <marco.neumann@be-binary.de>
2013-05-29 15:21:13 +0200

    Implementation of Bing Connection
    The connection to Bing is now implemented, the FIRST result is shown in
    our GUI.
    A class Result was implemented for storing the data from Bing.

---
+++ Web_Application/BingSearchConnector/BingSearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+using System.Net;
+
+namespace BingSearchConnector
+{
+    public class BingSearch
+    {
+        public static void startBingSearch(string aQueryString, out RequestHandler.Result[] aResults)
+        {
+            var aBingContainer = new Bing.BingSearchContainer(new Uri(rootUri));    //create container which handles the connection to Bing
+            aBingContainer.Credentials = new NetworkCredential(accountKey, accountKey);
+
+            //Build the query and execute it/send it to Bing
+            var aQuery = aBingContainer.Web(aQueryString, null, null, null, null, null, null, null);    //Query without any options, perhaps make them modifiable if useful
+            var aQueryResults = aQuery.Execute();    //synchronous call to Bing
+
+            //first get the number of results, than create the array, so there is no resizing necessary
+            //int aNumberOfResults = 0;
+            //foreach (var i in aQueryResults)
+            //{
+            //    aNumberOfResults++;
+            //}
+            aResults = new RequestHandler.Result[1];
+            // now store all the values in the array
+            int count = 0;
+            foreach (var aResult in aQueryResults)
+            {
+                if (count != 0)
+                    Array.Resize(ref aResults, aResults.Length + 1);
+                
+                aResults[count] = new RequestHandler.Result();
+                aResults[count].mGuid = aResult.ID;
+                aResults[count].mTitle = aResult.Title;
+                aResults[count].mDescription = aResult.Description;
+                aResults[count].mUrl = aResult.Url;
+                aResults[count].mDisplayUrl = aResult.DisplayUrl;
+            }
+
+        
+        }
+
+        private static string rootUri = "https://api.datamarket.azure.com/Bing/Search";
+        private static string accountKey = "mukZE9oHLj75rEkpKsRDXI6Aoa6DbzwoDyU9sRK0ahQ=";
+
+    }
+}
--- Web_Application/Web_Application/Search.cs
+++ Web_Application/Web_Application/Search.cs
@@ -9,6 +9,8 @@ namespace Web_Application
     {
         private string a;
 
+        //TODO: structure to store the results, should be replaced by the result class later
+
         public Search(string t)
         {
             a = t;
@@ -16,19 +18,16 @@ namespace Web_Application
 
         public string GetResults()
         {
-            return "String = "+a+"\nvestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellu porta."
-                    + "Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient moascetur ridiculus mus. "
-                    + "Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio gida atcursus necuctus a lorem. Maecenastrique orci ac sem.<br/>"
-                    + "Duis tempor velit nec orci vulputate ac elementum ligula faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices"
-                    + "posuere cubilia Curae; Aenean malesuadravida dolor nec accumsan. Fusce tincidunt odio ac sem laoreet nec rhoncus lacus consectetur?"
-                    + "Nunc a pellentesque mi. Mauris ullamcorper scelerisque nulla a facilisis. Nunc id tellus dui. Donec ultricies gravida fringilla. "
-                    + "Suspendisse cursus pretium eleifentudin, diam id lacinia bibendum, diam ligula feugiat felis, at porttitor."
-                    + "Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient moascetur ridiculus mus. "
-                    + "Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio gida atcursus necuctus a lorem. Maecenastrique orci ac sem.<br/>"
-                    + "Duis tempor velit nec orci vulputate ac elementum ligula faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices"
-                    + "posuere cubilia Curae; Aenean malesuadravida dolor nec accumsan. Fusce tincidunt odio ac sem laoreet nec rhoncus lacus consectetur?"
-                    + "Nunc a pellentesque mi. Mauris ullamcorper scelerisque nulla a facilisis. Nunc id tellus dui. Donec ultricies gravida fringilla. "
-                    + "Suspendisse cursus pretium eleifentudin, diam id lacinia bibendum, diam ligula feugiat felis, at porttitor.";
+
+
+            RequestHandler.Result[] aResults;
+
+            BingSearchConnector.BingSearch.startBingSearch(a, out aResults);
+
+            if (aResults[0] != null)  //if the first cell of the array is null, there were no results from Bing!
+                return "String = " + a + "<br />" + aResults[0].mTitle + "<br />" + aResults[0].mDescription;
+            else
+                return "String = " + a + "<br />" + "Sorry, no results!";
         }
     }
 }
