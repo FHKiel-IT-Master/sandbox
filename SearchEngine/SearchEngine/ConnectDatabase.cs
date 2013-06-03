4a98d67865462c7fb6b499b6943ed0ee253ba0f5
Ruchit Telrandhe <ruchit.telrandhe@gmail.com>
2013-06-03 23:15:13 +0200

    Rough Database Connection
    Please let me know when the interface for admin is done so that I can
    start integrating the interface with the database.

---
+++ SearchEngine/SearchEngine/ConnectDatabase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SearchEngine
+{
+    class ConnectDatabase
+    {
+        public void connection()
+        {
+            Form1 b = new Form1();
+            SqlConnection con = new SqlConnection("Data Source=Ruchit-PC;Initial Catalog=MetaSearchEngine;Integrated Security=True");
+
+            con.Open();
+
+
+            string insert = "INSERT INTO Context VALUES(" + b.textBox1.Text + ",'" + b.textBox2.Text + "','" + b.textBox3.Text + "'," + b.textBox4.Text + ");";
+            //            string modify = "UPDATE Context SET ID_Context='', Name='', Description='', Relation='';";
+            //            string delete = "DELETE FROM Context WHERE";
+
+            //            if insert_click
+            //           {
+            SqlCommand sc = new SqlCommand(insert, con);
+            sc.ExecuteNonQuery();
+            MessageBox.Show("The Context has been Added");
+            //           }
+
+            /*            if modify_click
+                        {
+                            SqlCommand sc = new SqlCommand( modify, con);
+                            sc.ExecuteNonQuery();
+                            MessageBox.Show("The Context has been Updated");
+                        }
+
+                        if delete_click
+                        {
+                            SqlCommand sc = new SqlCommand( delete, con);
+                            sc.ExecuteNonQuery();
+                            MessageBox.Show("The Context has been Deleted");
+                        }
+            */
+            con.Close();
+        }
+
+
+    }
+}
+             
---
+++ SearchEngine/SearchEngine/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace SearchEngine
+{
+    public class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ConnectDatabase a = new ConnectDatabase();
+
+            a.connection();
+
+
+/*            SqlConnection con = new SqlConnection("Data Source=Ruchit-PC;Initial Catalog=MetaSearchEngine;Integrated Security=True");
+
+            con.Open();
+            SqlCommand sc=new SqlCommand("insert into Context values("+ textBox1.Text +",'"+ textBox2.Text +"','"+ textBox3.Text +"',"+ textBox4.Text +");", con);
+               
+            sc.ExecuteNonQuery();
+
+
+            MessageBox.Show ("The Context has been Added");
+            con.Close();
+
+ */
+        }
+
+        public static void main (String[] args)
+        {
+            Application.Run(new Form1());
+
+        }
+
+    }
+}
